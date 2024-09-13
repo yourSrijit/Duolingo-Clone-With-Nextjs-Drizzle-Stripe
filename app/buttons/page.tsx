b35281dc769c608e7894f2e2fb29e16cb3ed885d
yourSrijit <berasrijit02@gmail.com>
2024-09-13 19:52:57 +0530

    Setup Done

---
+++ app/buttons/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function buttons(){
+
+    return (
+        <div className="p-4 space-y-4 felx flex-col max-w-[200px]">
+           <Button>Primary</Button>
+           <Button>Primary Outline</Button>
+        </div>
+    )
+}
+export default buttons
