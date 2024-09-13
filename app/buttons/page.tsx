7fe46ae19b6050d565747e649834f2472ef4c73c
yourSrijit <berasrijit02@gmail.com>
2024-09-13 20:39:51 +0530

    Button library added

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -4,8 +4,18 @@ function buttons(){
 
     return (
         <div className="p-4 space-y-4 felx flex-col max-w-[200px]">
-           <Button>Primary</Button>
-           <Button>Primary Outline</Button>
+           <Button>Default</Button>
+           <Button variant="primary">Primary</Button>
+           <Button variant="primaryOutline">Primary Outline</Button>
+           <Button variant="secondary">Secondary</Button>
+           <Button variant="secondaryOutline">Secondary Outline</Button>
+           <Button variant="danger">Danger</Button>
+           <Button variant="dangerOutline">Danger Outline</Button>
+           <Button variant="super">Super</Button>
+           <Button variant="superOutline">Super Outline</Button>
+           <Button variant="ghost">Ghost</Button>
+           <Button variant="sidebar">Sidebar</Button>
+           <Button variant="sidebarOutline">Sidebar Outline</Button>
         </div>
     )
 }
