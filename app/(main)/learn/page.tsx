27c2a5c11fe1e338c9fa60179e4d46cb60ffcb2b
yourSrijit <berasrijit02@gmail.com>
2024-09-16 13:59:41 +0530

    Flag added

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,6 +1,12 @@
+import { StickyWrapper } from "@/components/StickyWrapper"
+
 function LearnPage() {
   return (
-    <div>LearnPage</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        My Sticky Wrapper
+      </StickyWrapper>
+    </div>
   )
 }
 
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 // import { clerkMiddleware } from "@clerk/nextjs/server";
-import { authMiddleware } from "@clerk/nextjs/server";
+import {clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-    publicRoutes:["/"]
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
