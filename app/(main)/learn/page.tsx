ad48d5f4f1064599f83b5920ff33c23435aac5b5
yourSrijit <berasrijit02@gmail.com>
2024-09-18 21:03:55 +0530

    Dynamic Header done

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -10,6 +10,7 @@ async function LearnPage() {
   const userProgressData=getUserProgress();
 
   const [userProgress]=await Promise.all([userProgressData]);
+
   if(!userProgress || ! userProgress.activeCourse){
     redirect("/courses")
   }
@@ -17,15 +18,15 @@ async function LearnPage() {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
        <UserProgress
-       activeCourse={{title:"Spanish" ,imageSrc :"/es.png"}}
-       hearts={5}
-       points={100}
+       activeCourse={userProgress.activeCourse}
+       hearts={userProgress.hearts}
+       points={userProgress.points}
        hasActiveSubscription={false}
        />
       </StickyWrapper>
 
       <FeedWrapper>
-        <Header title="Spanish"/>
+        <Header title={userProgress.activeCourse.title}/>
       </FeedWrapper>
     </div>
   )
